a2bb3f420ad1db5fc5842c3e8585aa78972fdd48
Romi Kusuma Bakti <romikusumab@gmail.com>
2021-08-16 11:09:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Project UAS Java
---
+++ leaveboard/src/test/java/org/jarvis/leaveboard/LeaveboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jarvis.leaveboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
