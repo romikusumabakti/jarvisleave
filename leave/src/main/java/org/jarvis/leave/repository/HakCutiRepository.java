afd262c5b5c94d929fb4cf94ba512037344a223c
Tiaragustin <tiara237agustin@gmail.com>
2021-08-22 20:52:42 +0700

    Tambah DetailPengajuanCutiRepository

--- leave/src/main/java/org/jarvis/leave/repository/HakCutiRepository.java
+++ leave/src/main/java/org/jarvis/leave/repository/HakCutiRepository.java
@@ -1,4 +1,13 @@
 package org.jarvis.leave.repository;
 
-public interface HakCutiRepository {
+import org.jarvis.leave.model.HakCuti;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HakCutiRepository extends CrudRepository<HakCuti, Integer> {
+    @Override
+    @Query("select u from HakCuti u where u.isDeleted=false")
+    Iterable<HakCuti> findAll();
 }
