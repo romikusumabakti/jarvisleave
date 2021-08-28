f9ab2d25d67c7ffd13c6290cd22a70cc4b79ac98
diankhazaa <diannurul0827@gmail.com>
2021-08-28 11:26:11 +0700

    status cuti service

---
+++ src/main/java/org/jarvis/leave/service/StatusCutiService.java
@@ -0,0 +1,43 @@
+package org.jarvis.leave.service;
+
+import org.jarvis.leave.model.StatusCuti;
+import org.jarvis.leave.repository.StatusCutiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StatusCutiService {
+
+    StatusCutiRepository statusCutiRepository;
+
+    @Autowired
+    public StatusCutiService(StatusCutiRepository statusCutiRepository) {
+        this.statusCutiRepository = statusCutiRepository;
+    }
+
+    public List<StatusCuti> findAll() {
+        List<StatusCuti> statusCutis = new ArrayList<>();
+        statusCutiRepository.findAll().forEach(statusCutis::add);
+        return statusCutis;
+    }
+
+    public StatusCutiRepository findById(@PathVariable int id) {
+        return (StatusCutiRepository) statusCutiRepository.findById(id).orElse(null);
+    }
+
+    public StatusCuti saveOrUpdate(@RequestBody StatusCuti statusCuti) {
+        statusCutiRepository.save(statusCuti);
+        return statusCuti;
+    }
+
+    public void deleteById(@PathVariable int id) {
+        StatusCuti statusCuti = (StatusCuti) findById(id);
+        statusCuti.setIsDeleted(true);
+        statusCutiRepository.save(statusCuti);
+    }
+}
