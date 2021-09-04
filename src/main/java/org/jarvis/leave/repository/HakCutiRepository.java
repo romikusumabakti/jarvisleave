1a4f137e8ea4f4a2238f44f87dd298a6f04c65a3
Tiaragustin <tiara237agustin@gmail.com>
2021-09-04 23:15:55 +0700

    Mengganti CrudRepository dengan JpaRepository

--- src/main/java/org/jarvis/leave/repository/HakCutiRepository.java
+++ src/main/java/org/jarvis/leave/repository/HakCutiRepository.java
@@ -1,14 +1,17 @@
 package org.jarvis.leave.repository;
 
 import org.jarvis.leave.model.HakCuti;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HakCutiRepository extends CrudRepository<HakCuti, Integer> {
+public interface HakCutiRepository extends JpaRepository<HakCuti, Integer> {
 
     @Override
     @Query("select u from HakCuti u where u.isDeleted=false")
-    Iterable<HakCuti> findAll();
+    List<HakCuti> findAll();
 }
--- src/main/java/org/jarvis/leave/repository/JenisCutiRepository.java
+++ src/main/java/org/jarvis/leave/repository/JenisCutiRepository.java
@@ -1,14 +1,17 @@
 package org.jarvis.leave.repository;
 
 import org.jarvis.leave.model.JenisCuti;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface JenisCutiRepository extends CrudRepository<JenisCuti, Integer> {
+public interface JenisCutiRepository extends JpaRepository<JenisCuti, Integer> {
 
     @Override
     @Query("select u from JenisCuti u where u.isDeleted=false")
-    Iterable<JenisCuti> findAll();
+    List<JenisCuti> findAll();
 }
